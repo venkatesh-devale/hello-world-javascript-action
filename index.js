d2c484587d44e4f873ff9a07f2da04e60d162375
Venkatesh Devale <devalevenkatesh@gmail.com>
2022-01-05 23:55:58 -0800

    Fix time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}`);
-    const timme = (new Date()).toTimeString();
+    const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
